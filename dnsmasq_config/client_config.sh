51f5939c5ea62b563782ff5b393a3eaa987762bd
Parsac2002 <matiasscarpaglez@gmail.com>
2023-11-24 17:41:59 +0100

    changes in dnsmasq.conf and sccripts

---
+++ dnsmasq_config/client_config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# dir for client /tmp/pycore.1/client.conf/etc.dnsmasq.d
--- dnsmasq_config/dns_server_config.sh
+++ dnsmasq_config/dns_server_config.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+#/home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/dnsmasq.conf
+#/home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/ports.conf
+# dir for dns /tmp/pycore.1/dns-server.conf/etc.dnsmasq.d/
+echo "nameserver 127.0.0.1" > /tmp/pycore.1/dns-server.conf/etc/dnsmasq.conf/resolv.conf
+cp  /home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/dnsmasq.conf /tmp/pycore.1/dns-server.conf/etc/dnsmasq.conf
+killall dnsmasq
+/usr/sbin/dnsmasq -C /etc/dnsmasq.d/dnsmasq.conf
