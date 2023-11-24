cdd8d74f6468314ed23a19cefaa614aab7add262
Parsac2002 <matiasscarpaglez@gmail.com>
2023-11-24 18:03:26 +0100

    dns-server done

--- dnsmasq_config/dns_server_config.sh
+++ dnsmasq_config/dns_server_config.sh
@@ -2,7 +2,7 @@
 #/home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/dnsmasq.conf
 #/home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/ports.conf
 # dir for dns /tmp/pycore.1/dns-server.conf/etc.dnsmasq.d/
-echo "nameserver 127.0.0.1" > /tmp/pycore.1/dns-server.conf/etc/dnsmasq.conf/resolv.conf
-cp  /home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/dnsmasq.conf /tmp/pycore.1/dns-server.conf/etc/dnsmasq.conf
+echo "nameserver 127.0.0.1" > /etc/dnsmasq.d/resolv.conf
+cp  /home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/dnsmasq.conf /etc/dnsmasq.d
 killall dnsmasq
 /usr/sbin/dnsmasq -C /etc/dnsmasq.d/dnsmasq.conf
