c1991de83e42bc96d57bad310206c31363388157
Parsac2002 <matiasscarpaglez@gmail.com>
2023-11-24 11:38:33 +0100

    copied paths

--- dnsmasq_config/video-server_config.sh
+++ dnsmasq_config/video-server_config.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+# dir for dns /tmp/pycore.1/dns-server.conf/etc.dnsmasq.d
+# dirs for catalogue-server: 
+# /tmp/pycore.1/catalogue-server.conf/etc.apache2
+# /tmp/pycore.1/catalogue-server.conf/var.www
+
+# dirs for vide-server:
+# /tmp/pycore.1/video-server.conf/etc.apache2
+# /tmp/pycore.1/video-server.conf/var.www
