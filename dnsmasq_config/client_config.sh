932db8d2c06654f614e250cf2e9feb87859c3556
Parsac2002 <matiasscarpaglez@gmail.com>
2023-11-24 18:12:52 +0100

    dns_client_done

--- dnsmasq_config/client_config.sh
+++ dnsmasq_config/client_config.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-
+echo "nameserver 10.0.0.10" > /etc/dnsmasq.d/resolv.conf
+cp  /home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/dnsmasq.conf /etc/dnsmasq.d
+killall dnsmasq
+/usr/sbin/dnsmasq -C /etc/dnsmasq.d/dnsmasq.conf
 # dir for client /tmp/pycore.1/client.conf/etc.dnsmasq.d
