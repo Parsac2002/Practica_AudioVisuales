d63a0f2534349deb48d89aa8633067214595f05f
Parsac2002 <matiasscarpaglez@gmail.com>
2023-11-28 12:46:07 +0100

    client and dns config scripts changed

--- dnsmasq_config/client_config.sh
+++ dnsmasq_config/client_config.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
-echo "nameserver 10.0.0.10" > /etc/dnsmasq.d/resolv.conf
-cp  /home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/dnsmasq.conf /etc/dnsmasq.d
-killall dnsmasq
-/usr/sbin/dnsmasq -C /etc/dnsmasq.d/dnsmasq.conf
 # dir for client /tmp/pycore.1/client.conf/etc.dnsmasq.d
+
+# echo "nameserver 10.0.0.10" > /etc/dnsmasq.d/resolv.conf
+# cp  /home/parsac/AudioVisuales/Practica_AudioVisuales/dnsmasq_config/dnsmasq.conf /etc/dnsmasq.d
+# killall dnsmasq
+# /usr/sbin/dnsmasq -C /etc/dnsmasq.d/dnsmasq.conf
+
+touch /etc/dnsmasq.d/resolv.conf
+ 
+echo 'nameserver 10.0.0.10' >> /etc/dnsmasq.d/resolv.conf
+
+touch /etc/dnsmasq.d/dnsmasq.conf
+
+echo 'resolv-file=/etc/dnsmasq.d/resolv.conf' >> /etc/dnsmasq.d/dnsmasq.conf
+
+killall  dnsmasq
+/usr/sbin/dnsmasq -C /etc/dnsmasq.d/dnsmasq.conf
+
+#Despues de configurar el catalogue-server y el vide-server:
+# wget http://catalogue-server
+# wget http://video-server/movie.mp4
